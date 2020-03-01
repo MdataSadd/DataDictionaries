462198baffd2d6e4a4b925ac57197b52b778a2bb
Matthew Sadd <matthew@Matthews-MBP.lan>
2020-03-01 17:10:39 +0100

    Periodic included

--- Weights.py
+++ Weights.py
@@ -5,9 +5,17 @@ Molecular_Weights = {
                   "NaPF6":167.954,
                   "LiTFSI":287.075,
                   "LiFSI":187.07,
-                  "LiNO3":68.946
+                  "LiNO3":68.946,
                   }
 
 Atomic_Weights = {
-                "H":H.mass
+                "H":H.mass,
+                "Li":Li.mass,
+                "Na":Na.mass,
+                "K":K.mass,
+                "C":C.mass,
+                "N":N.mass,
+                "O":O.mass,
+                "F":F.mass,
+                "S":S.mass,
                 }
