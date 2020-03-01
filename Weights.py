1fe3645bd047f8645967d3bf4c769138015ce23d
Matthew Sadd <matthew@Matthews-MBP.lan>
2020-03-01 18:44:05 +0100

    Cleaning up small errors

--- Weights.py
+++ Weights.py
@@ -4,7 +4,7 @@ from periodictable import *
 #Due to funcitons contained in periodic table only chemical formula are needed
 
 Chemical_formulas = {
-                    "LiFTSI":"LiC2F6NO4S2",
+                    "LiTFSI":"LiC2F6NO4S2",
                     "LiFSI":"LiC2F2NO4S2",
                     "LiNO3":"LiNO3",
                     "LiPF6":"LiPF6",
@@ -13,4 +13,4 @@ Chemical_formulas = {
                     "PYR12FSI":"C7H16F2N2O4S2",
                     }
 # Examples of how to use code
-#print(formula(Chemical_formulas['LiTFSI']).mass)
+# print(formula(Chemical_formulas['LiTFSI']).mass)
