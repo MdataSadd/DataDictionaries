f28c9da57b72f4680edbfdd15af41a3a4d08a30c
Matthew Sadd <matthew@Matthews-MBP.lan>
2020-03-01 19:34:09 +0100

    Updated formulas dictionary

--- Weights.py
+++ Weights.py
@@ -3,14 +3,26 @@ from periodictable import *
 #Changes have been made to remove repeated functions
 #Due to funcitons contained in periodic table only chemical formula are needed
 
-Chemical_formulas = {
-                    "LiTFSI":"LiC2F6NO4S2",
-                    "LiFSI":"LiC2F2NO4S2",
-                    "LiNO3":"LiNO3",
-                    "LiPF6":"LiPF6",
-                    "NaPF6":"NaPF6",
-                    "PYR14TFSI":"C11H20N2F6S2O4",
-                    "PYR12FSI":"C7H16F2N2O4S2",
-                    }
+formulas = {
+            "TEGDME":"C10H22O5",
+            "DEGDME":"C6H14O3",
+            "DME":"C4H10O2",
+            "DOL":"C3H6O2",
+            "TOL":"C7H8",
+            "DMC":"C3H6O3",
+            "DEC":"C5H10O3",
+            "EC":"C3H4O3",
+            "PC":"C4H6O3",
+            "LiTFSI":"LiC2F6NO4S2",
+            "LiFSI":"LiC2F2NO4S2",
+            "LiNO3":"LiNO3",
+            "LiPF6":"LiPF6",
+            "NaPF6":"NaPF6",
+            "PYR14TFSI":"C11H20N2F6S2O4",
+            "PYR12FSI":"C7H16F2N2O4S2",
+            }
+
 # Examples of how to use code
+
+# How to get the molecular weight of a molecule
 # print(formula(Chemical_formulas['LiTFSI']).mass)
